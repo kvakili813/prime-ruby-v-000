e89055fdef0d49db9bcae6860714b15cd515a86f
Kayvan Vakili <kvakili813@gmail.com>
2018-02-28 02:11:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(integer)
-if integer < 0
-  false
-else
-  !((2..integer/2).to_a).any? { |int| integer%int == 0 }  
+  if integer < 0
+    false
+  else
+    !((2..integer/2).to_a).any? { |int| integer%int == 0 }
+  end
 end
